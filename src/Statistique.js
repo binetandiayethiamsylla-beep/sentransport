70a289cb29a8c4c4cffe9e259fa07969c8c5ce3c
Bineta Sylla <binetandiayethiamsylla@esp.sn>
2026-04-25 18:58:20 +0000

    Exercices : Footer, date Header, composant Statistique

---
+++ src/Statistique.js
@@ -0,0 +1,10 @@
+function Statistique() {
+  return (
+    <div className="statistique">
+      <h2 className="statistique-chiffre">10</h2>
+      <p className="statistique-libelle">Lignes</p>
+    </div>
+  );
+}
+
+export default Statistique;
